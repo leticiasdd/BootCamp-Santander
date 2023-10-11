1926dec6f9fe4d345e711c78900550013de9696c
Letícia Serrano <84883537+leticiasdd@users.noreply.github.com>
2023-10-11 14:33:45 -0300

    Create DesafioDio_2.py
    Descrição
    
    Após uma análise cuidadosa realizada pela equipe de desenvolvimento de uma empresa bancaria, foi identificado a necessidade de uma nova funcionalidade para otimizar os processos e melhorias da experiência dos usuários. Agora, sua tarefa é implementar uma solução que organize em ordem alfabética uma lista de ativos que será informada pelos usuários. Os ativos são representados por strings que representam seus tipos, como por exemplo: Reservas de liquidez, Ativos intangiveis e dentre outros.
    Entrada
    
    A primeira entrada consiste em um número inteiro que representa a  quantidade de ativos que o usuário possui. Em seguida, o usuário deverá  informar, em linhas separadas, os tipos (strings) dos respectivos ativos.
    Saída
    
    Seu programa deve exibir a lista de Ativos organizada em ordem alfabética. Cada ativo deve ser apresentado em uma linha separada.

---
+++ DesafioDio_2.py
@@ -0,0 +1,11 @@
+saldo_atual = float(input())
+valor_deposito = float(input())
+valor_retirada = float(input())
+
+#TODO: Calcular o saldo atualizado de acordo com a descrição deste desafio.
+
+saldoAtual += valorDeposito - valorRetirada
+
+#TODO: Imprimir o a saída de conforme a tabela de exemplos (uma casa decimal).
+
+print("Saldo atualizado na conta: {:.1f}".format(saldoAtual))
